0b09470fcedf65dea01a75fd1e23cc295e857fa9
yan zhong <yanzhong@yandeiMac.local>
2021-10-10 20:53:20 -0400

    get and post api are working

--- app.js
+++ app.js
@@ -1,6 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+const db = mongoose.connect('mongodb://localhost/bookAPI');
+const bookRouter = express.Router();
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+
+app.use(express.urlencoded({extended : true}));
+app.use(express.json())
+
+bookRouter.route('/books')
+  .post((req, res) => {
+    const book = new Book(req.body);
+    book.save(function(err){
+      if (err) console.error(err);
+    });
+    return res.status(200).json(book);
+  })
+  .get((req, res) => {
+    Book.find((err, books) =>{
+      if(err) {
+        return res.send(err);
+      }
+      return res.json(books);
+    });
+  });
+app.use('/api', bookRouter);
+
+
 app.get('/', (req, res) => {
   res.send('Welcome to my API!');
 });
